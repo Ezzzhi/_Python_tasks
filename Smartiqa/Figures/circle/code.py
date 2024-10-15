02cba6a5c962fb99e670246560e61948f75fcf7a
Alex <e_fridman@mail.ru>
2024-10-15 07:16:00 +0300

    15/10

---
+++ Smartiqa/Figures/circle/code.py
@@ -0,0 +1,10 @@
+from math import pi
+
+default_radius = 5
+
+def circle_perimeter(radius = default_radius):
+    return radius * 2 * pi
+    default_radius = 5
+
+def circle_area(radius = default_radius):
+    return radius ** 2 * pi
---
+++ Smartiqa/Moduls_packages.py
@@ -0,0 +1,5 @@
+import circle_perimeter() from circle
+print(circle_perimeter())
+print(circle_perimeter(10))
+print(circle_area())
+print(circle_area(10))
